ddaf745e2845aecce3f60773e9b8275748daddb9
exoter <exoter@esa.int>
2017-01-18 21:47:33 +0100

    Initial commit

---
+++ tasks/Task.cpp
@@ -0,0 +1,68 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "Task.hpp"
+
+using namespace camera_trigger;
+
+Task::Task(std::string const& name)
+    : TaskBase(name)
+{
+}
+
+Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
+    : TaskBase(name, engine)
+{
+}
+
+Task::~Task()
+{
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Task.hpp for more detailed
+// documentation about them.
+
+bool Task::configureHook()
+{
+    if (! TaskBase::configureHook())
+        return false;
+    return true;
+}
+bool Task::startHook()
+{
+    if (! TaskBase::startHook())
+        return false;
+    return true;
+}
+void Task::updateHook()
+{
+    TaskBase::updateHook();
+
+    bool triggerValue = false;
+    RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> frame;
+
+    // Check triggering condition (New sample & value TRUE)
+    if ((_trigger.read(triggerValue) == RTT::NewData) && (triggerValue == true))
+    {
+        // We got triggered, but we have to check if there is ANY sample at all (not necessarily a new one though) :)
+        // Pass frame from input to output
+        if (_frame_in.read(frame))
+        {
+            _frame_out.write(frame);
+        }
+    }
+}
+void Task::errorHook()
+{
+    TaskBase::errorHook();
+}
+void Task::stopHook()
+{
+    TaskBase::stopHook();
+}
+void Task::cleanupHook()
+{
+    TaskBase::cleanupHook();
+}
